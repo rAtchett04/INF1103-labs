21ecb56383f445d24d5ceb441b698d213c550f71
Haziq <mhr.sohan2004@gmail.com>
2026-09-04 15:13:45 +0800

    Lab 1 Post

--- hello.py
+++ hello.py
@@ -1,3 +1,38 @@
+# Activity 2
+'''
+print("======================")
+print("Welcome here!")
+print("My first post!")
+print("======================")
+
+username = "Haziq"
+bio = "SIT student"
+followers = 1000000
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+'''
+
+# Activity 3
+'''
+followers = 1000000
+
+followers += 500
+print("Day 2:", followers)
+
+followers += 500
+print("Day 3:", followers)
+
+followers += 500
+print("Day 4:", followers)
+
+followers += 500
+print("Day 5:", followers)
+'''
+ 
+# Activity 4
+''' 
 username = input("Enter username:")
 age = input("Enter age:")
 category = input("Enter content category:")
@@ -7,3 +42,18 @@ print("======================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+'''
+
+# Activity 5
+username = input("Enter username:")
+age = int(input("Enter age:"))
+category = input("Enter content category:")
+
+print("Instagram Profile")
+print("======================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+if age>40 and category == 'fun':
+    print("You are old what is fun for you?")
