e1f5af223bcac1f98372cdf05051a73e5b2e91cf
Haziq <mhr.sohan2004@gmail.com>
2026-09-04 14:45:51 +0800

    First Post

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 print("======================")
-print("whats up bro")
+print("Welcome!")
+print("My first post!")
+print("======================")
+
+username = "Haziq"
+bio = "SIT Student"
+followers = 1000000
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
